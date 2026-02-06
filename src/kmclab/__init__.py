e2c499fb767a139a15c3844476eb4cb7c93ffeaa
shirinasadix <123332493+shirinasadix@users.noreply.github.com>
2026-02-05 22:22:13 -0600

    Update __init__.py

--- src/kmclab/__init__.py
+++ src/kmclab/__init__.py
@@ -1,5 +1,5 @@
-from .hex import hex_kmc
+from .hexa import hexa_kmc
 from .square import square_kmc
 
-__all__ = ["hex_kmc", "square_kmc"]
+__all__ = ["hexa_kmc", "square_kmc"]
 __version__ = "0.1.0"
